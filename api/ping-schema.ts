1ca472308b958151a9041b727174060cf9816526
Nur Hasan Fadillah <nurhasanfadillah@gmail.com>
2026-06-20 06:50:13 +0700

    chore: remove diagnostic ping routes

--- api/ping-schema.ts
+++
@@ -1,6 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { transactions } from '../db/schema';
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  res.json({ ok: true, table: transactions._.name });
-}
